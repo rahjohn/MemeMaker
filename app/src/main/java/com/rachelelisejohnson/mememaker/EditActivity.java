aa92b04820471e077eba103f5fc8b7b5dede9b76
Tyler Johnson <tjohnson@control4.com>
2017-10-25 22:56:22 -0600

    Authentication stuff added

--- app/src/main/java/com/rachelelisejohnson/mememaker/EditActivity.java
+++ app/src/main/java/com/rachelelisejohnson/mememaker/EditActivity.java
@@ -12,9 +12,16 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.adobe.creativesdk.foundation.AdobeCSDKFoundation;
+import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
+
 import java.io.IOException;
 
-public class EditActivity extends AppCompatActivity {
+public class EditActivity extends AppCompatActivity implements IAdobeAuthClientCredentials {
+    private static final String CREATIVE_SDK_CLIENT_ID      = "9ac6ea61db4e404f81caa7eb1d8701b9";
+    private static final String CREATIVE_SDK_CLIENT_SECRET  = "76e7b3e0-9b37-43ad-ac73-0a52227416d7";
+    private static final String CREATIVE_SDK_REDIRECT_URI   = "ams+d9d6d1d8ac86f89db6db6bfbddbf1e550c011e46://adobeid/9ac6ea61db4e404f81caa7eb1d8701b9";
+    private static final String[] CREATIVE_SDK_SCOPES       = {"email", "profile", "address"};
 
     Bitmap bitmap = null;
 
@@ -41,6 +48,7 @@ public class EditActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AdobeCSDKFoundation.initializeCSDKFoundation(getApplicationContext());
         setContentView(R.layout.activity_edit);
 
         Intent intent = getIntent();
@@ -57,4 +65,24 @@ public class EditActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public String getClientID() {
+        return CREATIVE_SDK_CLIENT_ID;
+    }
+
+    @Override
+    public String getClientSecret() {
+        return CREATIVE_SDK_CLIENT_SECRET;
+    }
+
+    @Override
+    public String[] getAdditionalScopesList() {
+        return CREATIVE_SDK_SCOPES;
+    }
+
+    @Override
+    public String getRedirectURI() {
+        return CREATIVE_SDK_REDIRECT_URI;
+    }
 }
+
